10b842566a167618f0c2d6f79c899bc224211162
Michael Gutkin <miha011087@gmail.com>
2019-09-04 11:15:39 +0300

    return an error json if email already exists

--- api/account/create.js
+++ api/account/create.js
@@ -1,8 +1,28 @@
 const Account = require('../../models/account/Account');
 
+function validateEmail(email){
+
+	return Account.findOne({email: email}).then(function(result){
+		 return result !== null;
+	});
+ }
+
 module.exports = async function(req, res, next) {
+	var isValid;
 	const {email, name, age} = req.body;
 	const account = new Account({email, name, age});
-	await account.save();
-	return res.send({message: 'success'});
+	validateEmail(email).then(function(valid) {
+		this.isValid = valid;
+	  });
+
+	if (isValid) {
+		await account.save();
+		return res.send({message: 'success'});
+	} else {
+		return res.send({error: "email already exists"})
+	}
+
+	
+	
 };
+
